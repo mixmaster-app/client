a0ab4db8bab5c057487495caa22eede9d6387c1b
Kiiow <caillaud.hugo.44@gmail.com>
2025-04-20 23:44:22 +0200

    make the home page feel more alive

--- app/src/app/routes/home/home.component.ts
+++ app/src/app/routes/home/home.component.ts
@@ -1,8 +1,35 @@
 import { Component } from "@angular/core";
+import { random } from "../../../services/random";
 
 @Component({
     standalone: false,
     templateUrl: 'home.component.html',
     styleUrl: 'home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+    particles: any[] = [];
+
+    ngOnInit() {
+        this.particles = this.getLoopArray(150);
+    }
+
+    private getLoopArray(count: number): any[] {
+        return (Array(count)).fill(0).map(() => {
+            const particle = this.getRandomParticleValues();
+            return `--x:${particle.x}; --y:${particle.y}; --xt:${particle.xt}; --yt:${particle.yt}; --xd:${particle.xd}; --yd:${particle.yd}; --w:${particle.w}; --o:${particle.o};`;
+        });
+    }
+
+    private getRandomParticleValues() {
+        return {
+            x: random(0, 100),
+            y: random(0, 100),
+            xt: random(5, 20),
+            yt: random(5, 20),
+            xd: random(5, 20) / 10,
+            yd: random(5, 20) / 10,
+            w: random(35, 70),
+            o: random(1, 9) / 10
+        }
+    }
+}
--- app/src/app/routes/home/home.module.ts
+++ app/src/app/routes/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [LoginComponent],
+    imports: [LoginComponent, CommonModule],
     bootstrap: [HomeComponent],
 })
 export class HomeModule {}

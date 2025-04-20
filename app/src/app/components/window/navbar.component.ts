2ada5dae03ee9360d5e58244004c99e69591b111
Kiiow <caillaud.hugo.44@gmail.com>
2025-04-20 21:52:15 +0200

    Improve navbar and theme style

--- app/src/app/components/window/navbar.component.ts
+++ app/src/app/components/window/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'window-navbar',
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
     templateUrl: 'navbar.component.html',
     styleUrl: 'navbar.component.css'
 })
-export class WindowNavbarComponent {}
+export class WindowNavbarComponent {
+    @Input() title: string = "";
+    @Input() version: string = "";
+
+    getNavbarTitle() {
+        return `${this.title}${this.version != '' ? ` ${this.version}` : ''}`;
+    }
+}

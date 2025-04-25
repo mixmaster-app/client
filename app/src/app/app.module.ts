a156ef2609ecc70ede384f97e4bc8be6fd4f0d4b
Kiiow <caillaud.hugo.44@gmail.com>
2025-04-25 16:40:07 +0200

    Improve login component & add electron-store lib

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
 
 import { AppRoutesModule } from './app-routes.module';
 
@@ -21,7 +24,19 @@ import { HomeModule } from './routes/home/home.module';
         WindowNavbarComponent,
         WindowSidebarComponent,
     ],
-    providers: [],
+    providers: [
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Lara,
+                options: {
+                    prefix: 'p',
+                    darkModeSelector: 'none',
+                    cssLayer: false
+                }
+            }
+        })
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
--- app/src/app/routes/home/login/login.component.ts
+++ app/src/app/routes/home/login/login.component.ts
@@ -1,32 +1,56 @@
 import { Component } from "@angular/core";
-import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { FloatLabelModule } from "primeng/floatlabel";
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+
 import UserAccount from "@entity/launcher/user-account";
-import { createDefaultServer } from "@services/builder/ServerBuilder";
 import Server from "@entity/launcher/server";
 import Launcher from "@entity/launcher/launcher";
+import { createDefaultServer } from "@services/builder/ServerBuilder";
 import { createDefaultLauncher } from "@services/builder/LauncherBuilder";
 import { connect } from "@services/authenticate";
-import { FloatLabelModule } from "primeng/floatlabel";
-import { CommonModule } from "@angular/common";
 
 @Component({
     selector: 'login',
     imports: [
+        /* Angular */
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
+
+        /* Primeng */
         FloatLabelModule,
-        FormsModule
+        InputTextModule,
+        PasswordModule,
+        ButtonModule,
+        InputGroupModule,
+        InputGroupAddonModule
     ],
     templateUrl: 'login.component.html',
     styleUrl: 'login.component.css'
 })
 export class LoginComponent {
-    value1: string | undefined;
-    username: FormControl = new FormControl('');
-    password: FormControl = new FormControl('');
+    username: string = "";
+    password: string = "";
+    error: boolean = false;
+
+    constructor() {
+        
+    }
 
     login() {
-        var user: UserAccount = new UserAccount(this.username.value, this.password.value);
+        if(!this.username) {
+            this.error = true;
+        }
+        if(!this.password) {
+            this.error = true;
+        }
+        var user: UserAccount = new UserAccount(this.username, this.password);
         var server: Server = createDefaultServer();
         var launcher: Launcher = createDefaultLauncher();
         
